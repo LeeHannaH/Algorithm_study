713ee310863a25e933750dfdc31a5633827e0920
DESKTOP-5BVSVLC\user <konajii@naver.com>
2019-02-04 03:06:52 +0900

    - for문 조건을 sqrt로 줬더니 시간 복잡도가 줄어들었다! 기억하기!

---
+++ algorithmTest/src/algorithmTest/Solution11.java
@@ -0,0 +1,28 @@
+package algorithmTest;
+
+public class Solution11 {
+	 public static int solution(int N) {
+		 int factor = 0;
+		 int last_factor = N;
+		 
+		 if(N==1) factor++;
+		 for(int i=1; i<=Math.sqrt(N); i++) {
+			 if(i>=last_factor) break;
+			 if(N%i==0) {
+				 if(i!=N/i)
+					 factor+=2;
+				 else
+					 factor++;
+				 last_factor = N/i;
+			 }
+		 }
+		 System.out.println(factor);
+		 return factor;
+	 }
+	 public static void main(String[] args) {
+		 solution(1);
+		 solution(24);
+		 solution(100000);
+		solution(25);
+	}
+}
