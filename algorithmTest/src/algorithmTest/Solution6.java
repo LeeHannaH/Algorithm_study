8b24d1a7d538a42a6a8061e456087fc5f935755b
DESKTOP-5BVSVLC\user <konajii@naver.com>
2019-02-03 17:52:08 +0900

    - 스택 너무 헷갈려서 남의 코드 참고해서 겨우 함. 문제 풀 때 더 정확하게 알고리즘짠다음 코드로 옮기기

---
+++ algorithmTest/src/algorithmTest/Solution6.java
@@ -0,0 +1,64 @@
+package algorithmTest;
+
+import java.util.Stack;
+
+public class Solution6 {
+/*	public static int solution(int[] A, int[] B) {
+		int answer = 0;
+		int firstB = 0;
+		
+		if(B.length==1) return 1; 
+			
+		for(int i=0; i<B.length-1; i++) {
+			if(B[i]==1&&B[i+1]==0) {
+				firstB = i;
+				break;
+			}else {
+				answer++;
+			}
+		}
+		
+		for(int i=firstB+1; i<B.length; i++) {
+			if(B[firstB]!=B[i]) {
+				if(A[firstB]<A[i]) {
+					firstB = i;
+				}
+			}else {
+				answer++;
+			}
+			if(i==B.length-1) answer++;
+		}
+		System.out.println(answer);
+		return answer;
+    }*/
+	public static int solution(int[] A, int[] B) {
+		
+		Stack<Integer> downfish = new Stack<>();
+		int upfish = 0;
+		
+		for(int i=0; i<B.length; i++) {
+			if(B[i]==0) {
+				if(downfish.isEmpty()) {
+					upfish++;
+				}else {
+					if(A[i]>downfish.peek()) {
+						downfish.pop();
+					}else {
+						break;
+					}
+				}
+			}else {
+				downfish.push(A[i]);
+			}
+		}
+		
+		System.out.println(upfish+downfish.size());
+		return upfish+downfish.size();
+	}
+	public static void main(String[] args) {
+		int A[] = {4,3,2,1,5};
+		int B[] = {0,1,0,0,0};
+		
+		solution(A, B);
+	}
+}
