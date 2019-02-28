2ad9bd67f81d35c30c88564ea2057d79dc88dcce
DESKTOP-5BVSVLC\user <konajii@naver.com>
2019-02-28 23:11:53 +0900

    -2750

---
+++ algorithmTest/src/algorithmTest/algo2750MergeSort.java
@@ -0,0 +1,36 @@
+package algorithmTest;
+
+import java.util.Scanner;
+
+public class algo2750MergeSort {
+	
+	static int count=0;
+	int sorted[] = new int[count];
+	
+	public static void divide() {
+		
+		
+	}
+	public static void merge() {
+		
+	}
+	
+	
+	public static void main(String args[]) {
+		
+		Scanner sc = new Scanner(System.in);
+		count = sc.nextInt();
+		int array[] = new int[count];
+		for(int i=0; i<count; i++) {
+			array[i] = sc.nextInt();
+		}
+		sc.close();
+	
+	
+	
+	
+	
+	
+	
+	}
+}
