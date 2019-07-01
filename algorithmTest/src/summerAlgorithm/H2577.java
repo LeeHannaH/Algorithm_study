3a845d5c1b14301f72e0d9bd8733237f4f58ba68
DESKTOP-5BVSVLC\user <konajii@naver.com>
2019-07-01 16:32:59 +0900

    2577 풀었음

--- algorithmTest/src/summerAlgorithm/H2577.java
+++ algorithmTest/src/summerAlgorithm/H2577.java
@@ -1,5 +1,31 @@
 package summerAlgorithm;
 
-public class H2577 {
+import java.util.Scanner;
 
+public class H2577 {
+	public static void main(String[] args) {
+		int A, B, C;
+		int sum;
+		int[] num = new int[10];
+		
+		Scanner sc = new Scanner(System.in);
+		A = sc.nextInt();
+		B = sc.nextInt();
+		C = sc.nextInt();
+		
+		sum = A*B*C;
+		String sumWord = Integer.toString(sum);
+		
+		for(int i=0; i<num.length; i++) {
+			num[i] = 0;
+		}
+		
+		for(int i=0; i<sumWord.length(); i++) {
+			num[(int)sumWord.charAt(i)-48]++;
+		}
+		
+		for(int i=0; i<num.length; i++) {
+			System.out.println(num[i]);
+		}
+	}
 }
