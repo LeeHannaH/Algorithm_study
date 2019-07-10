1716841dbe102cfa9790554b37413c4e0790e517
DESKTOP-5BVSVLC\user <konajii@naver.com>
2019-07-10 23:37:36 +0900

    입력을 잘못넣었음,,

--- algorithmTest/src/summerAlgorithm/H1206.java
+++ algorithmTest/src/summerAlgorithm/H1206.java
@@ -13,31 +13,32 @@ public class H1206 {
 			}
 			
 			int ans = 0;
+	
+			// 좌2개 우2개 중에 제일 큰거 .. 를 어케 찾지 
+			// 만약 i일때 임시 arr만들어서 좌2우2개 넣고 
+			// 이걸 for문돌려서 max를 찾으면 될듯
 			
 			for(int i=0; i<apart; i++) {
-				
-				int maxNeigh = 0;
-				
-						if(maxNeigh < arr[i-2]) {
-							maxNeigh = arr[i-2];
-						}
-						if(maxNeigh < arr[i-1]) {
-							maxNeigh = arr[i-1];
-						}
-			
-						if(maxNeigh < arr[i+1]) {
-							maxNeigh = arr[i+1];
-						}
-						if(maxNeigh < arr[i+2]) {
-							maxNeigh = arr[i+2];
-						}
-						
-					if(arr[i]-maxNeigh >0) {
-						ans += arr[i]-maxNeigh;
+				int max = 0;
+				for(int k=i-2; k<=i+2; k++) {
+					if(k==i) continue; // 현재 i면 패스
+					if(i>1&&i<apart-2) { // 양끝인 0,1, n-2,n-1 패스
+					max = Math.max(max,arr[k]);
 					}
+				}
+				if(arr[i]-max>0) {
+					ans += (arr[i]-max);
+				}
 				
 			}
+			// 현재꺼 - 좌2개 우2개 중에 제일 큰거 > 0 이면
+			// 답 += 현재꺼 - 좌2개 우2개 중에 제일 큰거
+			
 			System.out.println("#"+(j+1)+" "+ans);
+			
 		}
 	}
 }
+
+//12
+//0 0 3 5 2 4 9 0 6 4 0 6 0 0
