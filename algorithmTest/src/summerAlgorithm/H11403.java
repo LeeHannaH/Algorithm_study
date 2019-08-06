46aaaeec1bf4294268dc7d99a00af149c079f20a
DESKTOP-5BVSVLC\user <konajii@naver.com>
2019-08-06 16:27:02 +0900

    백준 11403 조금 더 수정했다

--- algorithmTest/src/summerAlgorithm/H11403.java
+++ algorithmTest/src/summerAlgorithm/H11403.java
@@ -13,6 +13,7 @@ public class H11403 {
       N = sc.nextInt();
       arr = new int[N][N];
    
+      // 입력받아요
       ans = new int[N][N];
       for(int i=0; i<N; i++) {
          for(int j=0; j<N; j++) {
@@ -21,12 +22,12 @@ public class H11403 {
          }
       }
       
-      // arr[i][asdf] 가 1이면 가는길이 있다는거니 stack에 추가
-      // dfs해서 i에서 j까지 가는게 있는지 확인 후 있으면 1 없으면 0
+      // 시작점넣어서 dfs함수 돌리기
       for(int i=0; i<N; i++) {
             dfs(i);
       }
       
+      // 출력
       for(int i=0; i<N; i++) {
          for(int j=0; j<N; j++) {
             System.out.print(ans[i][j]+" ");
@@ -40,36 +41,24 @@ public class H11403 {
       boolean[] isvisited = new boolean[N];
       
       stack.add(new FindPoint(from));
-      System.out.println("----------------> stack에 "+from+"추가");
       while(!stack.isEmpty()) {
          int newfrom = stack.pop().from;
-         System.out.println("stack에서 뽑음:"+newfrom);
          for(int i=0; i<N; i++) {
-        	 System.out.println("------"+i+"-----------");
-        	 System.out.println("hasdfasdf");
-        	if(isvisited[i]) continue;
-            System.out.println("here???");
-            if(arr[newfrom][i] == 0) continue;
-            System.out.println("---i : "+i);
-            if(i==from) {
+        	if(isvisited[i]) continue; // 이미 간곳 continue
+            if(arr[newfrom][i] == 0) continue; // 갈 길이 없어도 continue
+            if(i==from) { // 원점으로 돌아왔으면 1로 고치고 continue
                ans[from][i] = 1;
-               System.out.println(from+"->"+i+" 추가");
                continue;
             }
-            ans[from][i] = 1;
-            System.out.println(i+", ");
-            if(arr[newfrom][i] == 1) {
-            	isvisited[i] = true;
-            	System.out.println("---------------->stack에 "+i+"추가");
-               stack.push(new FindPoint(i));
-            }
-            
-         }System.out.println();
+            ans[from][i] = 1; // from->somewhere 1로 바꿔주고
+            isvisited[i] = true;
+            stack.push(new FindPoint(i));
+         }
       }
       
    }
 }
-
+// 만들필요 없었는데 그냥 해씀
 class FindPoint{
    int from;
    FindPoint(int from){
