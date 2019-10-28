22fd4a4fab0ad40b15598e00882666ceae440738
DESKTOP-5BVSVLC\user <konajii@naver.com>
2019-10-28 21:49:55 +0900

    9999 무한루프는 해결했음(string비교), 테케 10,11 해결못함

--- algorithmTest/src/haban/MakeBigNum.java
+++ algorithmTest/src/haban/MakeBigNum.java
@@ -7,21 +7,29 @@ public class MakeBigNum {
 		System.out.println(solution("1231234", 3));
 		System.out.println(solution("4177252841", 4));
 		System.out.println(solution("9994994", 3));
+		System.out.println(solution("1111", 3));
 		
 	}
     public static String solution(String number, int k) {
         StringBuffer str = new StringBuffer(number);
+        StringBuffer str2 = new StringBuffer("");
         String answer ="";
         int count  = k;
-        boolean doSomething = false;
+        
         
         while(count>0) {
-            
-        	
-        	
+    
 	        for(int i=1; i<str.length(); i++) {
-
-
+	        	
+	        	if(i==str.length()-1) {
+		        	if(str!=str2) {
+		        		str2 = str;
+		        	}else {
+		        		str.delete(str.length()-count, str.length());
+		        		count=0;
+		        		break;
+		        	}
+	        	}
 	        	if(str.charAt(i-1)<str.charAt(i)) {
 	        		if(count==0) {
 	        			answer = str.toString();
@@ -29,7 +37,6 @@ public class MakeBigNum {
 	        		}
 	        		str.deleteCharAt(i-1);
 	        		count--;
-	        		doSomething = true;
 	        		
 	        		break;
 	        	}
